6116ec304f75698d80f8a3e6a895d9326a58b73e
hdigiorgi <me@hdigiorgi.com>
2019-02-12 13:03:52 -0300

    timestamp api

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var express = require('express');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// so that your API is remotely testable by FCC
 var cors = require('cors');
 app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
 
@@ -19,12 +19,18 @@ app.get("/", function (req, res) {
 });
 
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+// your first API endpoint...
+app.get("/api/timestamp/:date_string?", function (req, res) {
+  const inputString = req.params.date_string || '';
+  const inputNumber = parseInt(inputString);
+  var date = inputNumber ? new Date(inputNumber) :
+             inputString !== ''? new Date(inputString):
+             new Date();
+  res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString()
+  });
 });
-
-
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
